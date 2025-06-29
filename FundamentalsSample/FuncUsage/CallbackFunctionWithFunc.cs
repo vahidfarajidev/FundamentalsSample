26f7a4cf923ff526eea2556c86ed562cacf7d02c
Vahid Faraji <vahid_faraji83@yahoo.com>
2025-06-29 21:30:38 +0200

    Add callback implementation using Func delegate.

---
+++ FundamentalsSample/FuncUsage/CallbackFunctionWithFunc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalsSample.FuncUsage.CallbackFunctionWithFunc
+{
+    internal class FileDownloader
+    {
+        public void Download(string fileUrl, Func<int, string> onProgress)
+        {
+
+            for (int i=1; i<=10; i++)
+            {
+                Thread.Sleep(300);
+                Console.WriteLine(onProgress(i * 10));
+            }
+
+            Console.WriteLine("Download complete!");
+        }
+    }
+}
--- FundamentalsSample/Program.cs
+++ FundamentalsSample/Program.cs
@@ -1,9 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
-using FundamentalsSample.FuncUsage.InjectingLogicWithFunc;
+//using FundamentalsSample.FuncUsage.InjectingLogicWithFunc;
+//var processor = new PaymentProcessor(amount => amount < 1000);
+//processor.Pay(500);
 
-var processor = new PaymentProcessor(amount => amount < 1000);
-processor.Pay(500);
+using FundamentalsSample.FuncUsage.CallbackFunctionWithFunc;
+var downloader = new FileDownloader();
+downloader.Download("http://example.com/file.zip", percent => $"Progress: {percent}%");
 
 Console.ReadLine();
