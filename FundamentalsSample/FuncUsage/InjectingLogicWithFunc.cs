62eef7b94eb66cce50daae23b808ba3b77871b10
Vahid Faraji <vahid_faraji83@yahoo.com>
2025-06-29 20:57:38 +0200

    Add project files.

---
+++ FundamentalsSample/FuncUsage/InjectingLogicWithFunc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalsSample.FuncUsage.InjectingLogicWithFunc
+{
+    internal class PaymentProcessor
+    {
+        private readonly Func<decimal, bool> _validate;
+
+        public PaymentProcessor(Func<decimal, bool> validateStrategy)
+        {
+            _validate = validateStrategy;
+        }
+
+        public void Pay(decimal amount)
+        {
+            if (_validate(amount))
+                Console.WriteLine("Payment accepted.");
+            else
+                Console.WriteLine("Payment rejected");
+        }
+    }
+}
---
+++ FundamentalsSample/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using FundamentalsSample.FuncUsage.InjectingLogicWithFunc;
+
+var processor = new PaymentProcessor(amount => amount < 1000);
+processor.Pay(500);
+
+Console.ReadLine();
