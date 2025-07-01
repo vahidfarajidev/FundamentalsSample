636235d9c2f2d7185b1cc97d8cf4fd2406f7c79e
Vahid Faraji <vahid_faraji83@yahoo.com>
2025-07-01 19:29:32 +0200

    Add Event Handling (Action used with Events).

---
+++ FundamentalsSample/FuncUsage/EventHandlingWithAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FundamentalsSample.FuncUsage.EventHandlingWithAction
+{
+    public class Button
+    {
+        public event Action<string> Clicked;
+        
+        public void SimulateClick()
+        {
+            Clicked?.Invoke("Button1");
+        }
+    }
+}
--- FundamentalsSample/Program.cs
+++ FundamentalsSample/Program.cs
@@ -1,20 +1,31 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //Console.WriteLine("Hello, World!");
 
+
 //using FundamentalsSample.FuncUsage.InjectingLogicWithFunc;
 //var processor = new PaymentProcessor(amount => amount < 1000);
 //processor.Pay(500);
 
-using FundamentalsSample.FuncUsage.CallbackFunctionWithFunc;
+
+//using FundamentalsSample.FuncUsage.CallbackFunctionWithFunc;
 //var downloader = new FileDownloader();
 //downloader.Download("http://example.com/file.zip", percent => $"Progress: {percent}%");
 
-var validator = new InputValidator();
-string result = validator.Validate("hello123", value => value.Length >= 5 && value.All(char.IsLetterOrDigit));
-Console.WriteLine(result);
+//var validator = new InputValidator();
+//string result = validator.Validate("hello123", value => value.Length >= 5 && value.All(char.IsLetterOrDigit));
+//Console.WriteLine(result);
 
-result = validator.Validate("hell@", value => value.All(char.IsLetterOrDigit));
-Console.WriteLine(result);
+//result = validator.Validate("hell@", value => value.All(char.IsLetterOrDigit));
+//Console.WriteLine(result);
+
+
+
+using FundamentalsSample.FuncUsage.EventHandlingWithAction;
+
+var button = new Button();
+button.Clicked += name => Console.WriteLine($"{name} clicked!");
+
+button.SimulateClick(); // Output: Button1 clicked!
 
 
 Console.ReadLine();
